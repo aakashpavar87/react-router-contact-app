7d0c4b16e1b036ee1a3c286d7d2e22e1eddd6c8b
aakashpavar87 <aakashpavar87@gmail.com>
2024-01-25 23:20:43 +0530

    Initial Commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,15 @@
+export default function Index() {
+    return (
+    <>    
+        <h1>
+            My Contacts
+        </h1>
+        <p id="zero-state">
+            This is a demo for React Router.
+            <br />
+            Check out{" "}
+            .
+        </p>
+    </>
+    );
+}
